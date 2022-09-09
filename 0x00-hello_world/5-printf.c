0ae8034dfec8606abfbf1a9f377f7a61ae737caa
inifavour <iniobongetuk22@gmail.com>
2022-09-08 20:52:16 -0700

    Hello, printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the string will be put in the printf function
+ *
+ * program prints string with proper grammar, but the outcome is a piece of art
+ * Return: it gives 0 successful
+ */
+int main(void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
