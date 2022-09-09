dd7fe50d8560ff92b0f4089af47bdd788852112c
inifavour <iniobongetuk22@gmail.com>
2022-09-08 23:26:50 -0700

    5-printf.c updated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+
+/**
+ * main- It prints out a text string
+ *
+ * Return: 1 is successful
+ */
+int main(void)
+{      
+	write(2,"and that piece of art is useful\"-Dora Korpar,2015-10-19\n",59);
+	return (1);
+}
