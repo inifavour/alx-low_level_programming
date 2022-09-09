c97cf66dac0e6bf07f6e806eefb90a5c46cdf9ea
inifavour <iniobongetuk22@gmail.com>
2022-09-08 21:40:21 -0700

    Intel file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - the string will be put in the printf function
- *
- * program prints string with proper grammar, but the outcome is a piece of art
- * Return: it gives 0 successful
- */
+* main - the string will be put in the printf function
+*
+* program prints string successfully
+* Return: it gives 0 successful
+*/
 int main(void)
-{	
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+{
+ 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>	
+
+/**
+ * main	- print out string using printf
+ *
+ * Description	- use of the function main
+ * the program prints out the function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long int 
