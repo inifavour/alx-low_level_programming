3d6207333f2b0f227eaa53b65cbe5f57e49809ed
inifavour <iniobongetuk22@gmail.com>
2022-09-11 12:33:12 -0700

    updated file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - prints all possible combinations of single-digit numbers
  *
  * Return: Always 0
@@ -21,6 +20,6 @@ int main(void)
 	}
 
 	putchar('\n');
-
+	
 	return (0);
 }
