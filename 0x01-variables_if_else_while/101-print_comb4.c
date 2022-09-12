f220b379103eefcf2708a1ddc4a15b7393226640
inifavour <iniobongetuk22@gmail.com>
2022-09-12 02:58:07 -0700

    updated file 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,22 +9,26 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 0; num1 < 8; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = 0; num2 < 9; num2++)                                                {
-	 		for (num3 = 0; num2 < 10; num3++) 
+		for (num2 = 1; num2 < 9; num2++)                                                {
+		{
+	 		for (num3 = 2; num2 < 9; num3++) 
 			{
-				putchar((num1 % 10) + '0');
-				putchar((num2 % 10) + '0');
-				putchar((num3 % 10) + '0');
-
-				if (num1 == 7 && num2 == 8 && num3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(',');
+			if (num3 > num2 && num2 > num1)
+			{
+				putchar(num1 + '0');
+				putchar(num2 + '0');
+				putchar(num3 + '0');
+					if(i != 7)
+					{
+						putchar(',');
+						putchar(',');
+					}
+			}
 			}
 		}
+		
 	}
 	
 	putchar('\n');
