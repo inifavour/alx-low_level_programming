f280eec70194da026e7b9f2ab05a3f83051618e4
inifavour <iniobongetuk22@gmail.com>
2022-09-30 13:19:05 -0700

    factorial file

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,16 +9,16 @@
  */
 int factorial(int n);
 {
-	int result = n;
-
-     if (n < 0)
+	if (n < 0)
 	{
-		  return (-1);
+		return (-1);
 	}
-
-	  else if (n >= 0 && n <= 1)
-	  {
-		   return (1);
-	  }
-
+	else if (n < 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
