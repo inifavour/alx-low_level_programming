511cfdde80624f6969c3ac22a7ac8221fc929f49
inifavour <iniobongetuk22@gmail.com>
2022-10-03 10:56:08 -0700

    length file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+
+{
+	int i;
+	char Str[1000];
+
+	for (i = 0; Str[i] != '\0'; ++i)
+
+	return (i);
+}
