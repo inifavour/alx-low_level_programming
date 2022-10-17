7a077dec0d203fb8a95b61946a64d58978798ebf
inifavour <iniobongetuk22@gmail.com>
2022-10-17 05:10:52 -0700

    updated sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,6 +12,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	va_list ptr;
 	va_start(ptr, n);
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ptr, int);
