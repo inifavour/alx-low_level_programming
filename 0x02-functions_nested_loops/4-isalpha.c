05da485450f3cf2be772977202b7f4c238c961cc
inifavour <iniobongetuk22@gmail.com>
2022-10-17 11:55:35 -0700

    updated file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,14 +2,13 @@
 
 /**
  * _isalpha - checks for alphabetic character
- *
+ *  @c: type int character
  * Return: 1 if it is a letter otherwise 0
  */
 int _isalpha(int c)
 {
-	int c;
-	if ((c >= 'a' && c <= 'z') ||
-	   (c >= 'A' && c <= 'Z'))
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
