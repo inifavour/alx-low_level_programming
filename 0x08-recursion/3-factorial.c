3d8cb67e939b5be76f29e917a2a71edb465e5599
inifavour <iniobongetuk22@gmail.com>
2022-10-25 03:33:33 -0700

    factorial numbers

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,17 +9,15 @@
  */
 int factorial(int n);
 {
+	int fact;
 
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n < 2)
+	if (n == 1)
 	{
 		return (1);
 	}
 	else
 	{
-		return (n * factorial(n - 1));
+		fact = n * factorial(n - 1);
+		return (fact);
 	}
 }
